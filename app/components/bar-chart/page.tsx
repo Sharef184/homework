8258840bc678154bf6a693316db8eca251ac3deb
DESKTOP-Q7CBIR9\DELL <sharifkhlief@gmail.com>
2023-11-14 06:25:13 +0100

    Dashboard-Fix

--- app/components/bar-chart/page.tsx
+++ app/components/bar-chart/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { useState, useEffect } from 'react'
-import { Bar } from 'react-chartjs-2'
+import React from 'react';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -9,7 +8,8 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js'
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 
 ChartJS.register(
   CategoryScale,
@@ -20,45 +20,35 @@ ChartJS.register(
   Legend
 );
 
-const BarChart = () => {
-  const [chartData, setChartData] = useState({
-    datasets: [],
-  });
-
-  const [chartOptions, setChartOptions] = useState({});
-
-  useEffect(() => {
-    setChartData({
-        labels: ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'],
-        datasets: [
-            {
-                label: 'Death New',
-                data: [87, 169, 126, 185, 153, 62, 55],
-                borderColor: 'rgb(53, 162, 235)',
-                backgroundColor: 'rgb(85, 156, 143, 0.7',
-            }, 
-        ]
-    })
-    setChartOptions({
-        plugins: {
-            legend: {
-                position: 'top',
-            },
-            title: {
-                display: true,
-                // text: 'Daily Data'
-            }
-        },
-        maintainAspectRatio: false,
-        responsive: true
-    })
-  }, [])
-
-  return (
-      <div className='p-2'>
-        <Bar data={chartData} options={chartOptions} />
-      </div>
-  );
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+   // text: 'Daily Data'
+    },
+  },
 };
+
+const labels = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Death New',
+      data: [87, 169, 126, 185, 153, 62, 55],
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgb(85, 156, 143, 0.7',
+    },
+  ],
+};
+
+export function BarChart() {
+  return <Bar options={options} data={data} />;
+}
 
 export default BarChart;
